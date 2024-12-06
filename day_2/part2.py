ed3d7323794ba0372107eafac703de5936c3f555
mzelder <maciej.zelder@wp.pl>
2024-12-06 12:12:04 +0100

    solved part2

--- day_2/part2.py
+++ day_2/part2.py
@@ -5,31 +5,25 @@ def main():
             numbers = line.split(" ")
             numbers = [int(number.rstrip("\n")) for number in numbers]
 
-            report = check_levels(numbers)
-            if report:
-                safe_reports += 1
+            for i in range(len(numbers)):
+                copy_numbers = numbers.copy()
+                del copy_numbers[i]
+                report = check_report(copy_numbers)
+                if report:
+                    safe_reports += 1
+                    break
 
     print(safe_reports)
 
-def check_levels(numbers, wrong_index=None):
-    cp_numbers = numbers
-    if wrong_index: 
-        del numbers[wrong_index]
-
+def check_report(numbers):
     increase = numbers[0] < numbers[1]
     current_number = numbers[0]
     for i in range(1, len(numbers)):
         if not (1 <= abs(abs(current_number)  - abs(numbers[i])) <= 3):
-            if not wrong_index:
-                return check_levels(cp_numbers, i)
             return False
         elif increase and current_number > numbers[i]:
-            if not wrong_index:
-                return check_levels(cp_numbers, i)
             return False
         elif not increase and current_number < numbers[i]:
-            if not wrong_index:
-                return check_levels(cp_numbers, i)
             return False
         
         current_number = numbers[i]
