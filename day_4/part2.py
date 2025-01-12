5d876a8e72e5f3d1c49ef764b487b5e928be31e8
mzelder <maciej.zelder@wp.pl>
2025-01-12 16:36:30 +0100

    Day 4 finished

--- day_4/part2.py
+++ day_4/part2.py
@@ -1,30 +1,44 @@
-import re
-
-result = 0
-
 def main():
-    with open("test2.txt") as f:
+    with open("problem4.txt") as f:
         file = f.read().splitlines()
 
-    line_length = len(file[0])
-    i = 0
-    j = 3
-    array = []
-    while i < len(file) - 2:
-        array.append([])
-        while j < line_length:
-            three_grid_array = []
-            three_grid_array.append(file[i][j-3:j])
-            three_grid_array.append(file[i+1][j-3:j])
-            three_grid_array.append(file[i+2][j-3:j])
-            print(three_grid_array)
-            array[i].append(three_grid_array)
-            j += 1
-        i += 1
+    result = 0
+    row_len = len(file[0])
+    height_len = len(file)   
     
-    print(array)
-def find_matching_words():
-    pass 
+    for _ in range(height_len - 2):
+        start = 0
+        end = 3
+        
+        for i in range(row_len - 2):
+            matrix = get_matrix_3_by_3(file, start, end)
+            if check_x_mas(matrix):
+                result += 1
+            start += 1 
+            end += 1
+        
+        del file[0]
+    
+    print(result)
 
+def get_matrix_3_by_3(file, start, end):
+    result = []
+    
+    for i, line in enumerate(file):
+        result.append(line[start:end])
+        if i == 2:
+            break
+    return result
+
+def check_x_mas(matrix):
+    first, second, third = matrix[0], matrix[1], matrix[2]
+    left_word = first[0] + second[1] + third[2]
+    right_word = first[2] + second[1] + third[0]
+    
+    left = left_word in ("MAS", "SAM")
+    right = right_word in ("MAS", "SAM")
+
+    return (left and right)
+    
 if __name__ == "__main__":
     main()
