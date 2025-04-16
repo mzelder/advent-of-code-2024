58b0e46be2d5fe8548141692f4af6471b6613697
mzelder <maciej.zelder@wp.pl>
2025-04-16 09:06:59 +0200

    day6 c# partly solved

---
+++ day_6/part1/Program.cs
@@ -0,0 +1,37 @@
+﻿List<string> lines = new List<string>();
+using (StreamReader sr = new StreamReader("../test1.txt")) {
+    string? line;
+    while ((line = sr.ReadLine()) != null) {
+        lines.Add(line);
+    }
+}
+(int line, int index) currentPosition = findStartingPosition();
+int direction = 1; // 0 left, 2 right, 1 up, 3 down;
+
+(int, int) findStartingPosition() {
+    int line = 0;
+    int index = 0;
+    char currentChar;
+    string currentStr;
+    char desireChar = '^';
+
+    while (line != lines.Count) {
+        currentStr = lines[line];
+        while (index != currentStr.Length) {
+            if ((currentChar = currentStr[index]) == desireChar) {
+                return (line, index);
+            }
+            index++;
+        }
+        index = 0; 
+        line++;
+    }
+
+    return (line, index);
+}
+
+void moveUP() {
+    while (true) {
+        
+    }
+}
